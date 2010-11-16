9dd61c299a3663b19bcd51f60bab3520030d2020
damnmad <damnmad@b53aeedb-cb62-44e9-bba4-a92948f050a7>
2010-11-16 12:30:14 +0000

    git-svn-id: http://95.142.168.194/svn/Uralys/TribesAndKhans/trunk@43 b53aeedb-cb62-44e9-bba4-a92948f050a7

---
+++ src/com/uralys/tribes/entities/dto/ProfilDTO.java
@@ -0,0 +1,51 @@
+package com.uralys.tribes.entities.dto;
+
+import java.util.List;
+
+import javax.jdo.annotations.Extension;
+import javax.jdo.annotations.IdGeneratorStrategy;
+import javax.jdo.annotations.IdentityType;
+import javax.jdo.annotations.PersistenceCapable;
+import javax.jdo.annotations.Persistent;
+import javax.jdo.annotations.PrimaryKey;
+
+@PersistenceCapable(identityType = IdentityType.APPLICATION)
+public class ProfilDTO {
+
+
+    @PrimaryKey
+    @Persistent(valueStrategy = IdGeneratorStrategy.IDENTITY)
+    @Extension(vendorName="datanucleus", key="gae.encoded-pk", value="true")	
+    protected String key;
+
+	@Persistent
+    @Extension(vendorName="datanucleus", key="gae.pk-name", value="true")
+	private String uralysUID;
+
+	@Persistent	private List<String> playerUIDs;
+	
+	
+	//-----------------------------------------------------------------------------------//
+
+	public String getKey() {
+		return key;
+	}
+	public void setKey(String key) {
+		this.key = key;
+	}
+	public String getUralysUID() {
+		return uralysUID;
+	}
+	public void setUralysUID(String uralysUID) {
+		this.uralysUID = uralysUID;
+	}
+	public List<String> getPlayerUIDs() {
+		return playerUIDs;
+	}
+	public void setPlayerUIDs(List<String> playerUIDs) {
+		this.playerUIDs = playerUIDs;
+	}
+	
+	//-----------------------------------------------------------------------------------//
+	
+}

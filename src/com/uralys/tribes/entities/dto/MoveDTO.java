b489cf5bccb84508d7e5d5a630c5f5448c9221c0
damnmad <damnmad@b53aeedb-cb62-44e9-bba4-a92948f050a7>
2010-11-17 16:36:15 +0000

    git-svn-id: http://95.142.168.194/svn/Uralys/TribesAndKhans/trunk@47 b53aeedb-cb62-44e9-bba4-a92948f050a7

---
+++ src/com/uralys/tribes/entities/dto/MoveDTO.java
@@ -0,0 +1,76 @@
+package com.uralys.tribes.entities.dto;
+
+import javax.jdo.annotations.Extension;
+import javax.jdo.annotations.IdGeneratorStrategy;
+import javax.jdo.annotations.IdentityType;
+import javax.jdo.annotations.PersistenceCapable;
+import javax.jdo.annotations.Persistent;
+import javax.jdo.annotations.PrimaryKey;
+
+@PersistenceCapable(identityType = IdentityType.APPLICATION)
+public class MoveDTO {
+
+
+    @PrimaryKey
+    @Persistent(valueStrategy = IdGeneratorStrategy.IDENTITY)
+    @Extension(vendorName="datanucleus", key="gae.encoded-pk", value="true")	
+    protected String key;
+
+	@Persistent
+    @Extension(vendorName="datanucleus", key="gae.pk-name", value="true")
+	private String moveUID;
+
+	@Persistent private PlayerDTO player;
+	@Persistent private int xFrom;
+	@Persistent private int xTo;
+	@Persistent private int yFrom;
+	@Persistent private int yTo;
+	
+	//-----------------------------------------------------------------------------------//
+
+	public String getKey() {
+		return key;
+	}
+	public void setKey(String key) {
+		this.key = key;
+	}
+	public String getMoveUID() {
+		return moveUID;
+	}
+	public void setMoveUID(String moveUID) {
+		this.moveUID = moveUID;
+	}
+	public PlayerDTO getPlayer() {
+		return player;
+	}
+	public void setPlayer(PlayerDTO player) {
+		this.player = player;
+	}
+	public int getxFrom() {
+		return xFrom;
+	}
+	public void setxFrom(int xFrom) {
+		this.xFrom = xFrom;
+	}
+	public int getxTo() {
+		return xTo;
+	}
+	public void setxTo(int xTo) {
+		this.xTo = xTo;
+	}
+	public int getyFrom() {
+		return yFrom;
+	}
+	public void setyFrom(int yFrom) {
+		this.yFrom = yFrom;
+	}
+	public int getyTo() {
+		return yTo;
+	}
+	public void setyTo(int yTo) {
+		this.yTo = yTo;
+	}
+	
+	//-----------------------------------------------------------------------------------//
+	
+}
--- src/tests/TribesAndKhansServlet.java
+++ src/tests/TribesAndKhansServlet.java
@@ -11,6 +11,7 @@ import javax.servlet.http.*;
 import com.google.appengine.api.datastore.Key;
 import com.google.appengine.api.datastore.KeyFactory;
 import com.uralys.tribes.dao.impl.PMF;
+import com.uralys.tribes.entities.dto.MoveDTO;
 import com.uralys.tribes.entities.dto.PlayerDTO;
 import com.uralys.tribes.entities.dto.ProfilDTO;
 import com.uralys.utils.Utils;
@@ -19,55 +20,75 @@ import com.uralys.utils.Utils;
 public class TribesAndKhansServlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws IOException {
+		
 		PersistenceManager pm = PMF.getInstance().getPersistenceManager();
-		
-		String uralysUID = Utils.generateUID();
-		ProfilDTO profil = new ProfilDTO();		
-		Key key = KeyFactory.createKey(ProfilDTO.class.getSimpleName(), uralysUID);
-		
-		profil.setKey(KeyFactory.keyToString(key));
-		profil.setUralysUID(uralysUID);
-		
-		
-		String playerUID1 = Utils.generateUID();
-		PlayerDTO player1 = new PlayerDTO();		
-		Key key2 = KeyFactory.createKey(PlayerDTO.class.getSimpleName(), playerUID1);
-		String playerUID2 = Utils.generateUID();
-		PlayerDTO player2 = new PlayerDTO();		
-		Key key3 = KeyFactory.createKey(PlayerDTO.class.getSimpleName(), playerUID2);
-		
-		player1.setKey(KeyFactory.keyToString(key2));
-		player1.setPlayerUID(playerUID1);
-		player1.setName(Utils.generatePassword());
-
-		player2.setKey(KeyFactory.keyToString(key3));
-		player2.setPlayerUID(playerUID2);
-		player2.setName(Utils.generatePassword());
-		
-		
-		profil.getPlayers().add(player1);
-		profil.getPlayers().add(player2);
-		
-		pm.makePersistent(profil);
-		pm.flush();
-		pm.refreshAll();
-
-		//===================================================================//
-		PrintWriter out = resp.getWriter();
-		
-		Query q = pm.newQuery("select from " + ProfilDTO.class.getName());
 
 		
-		@SuppressWarnings("unchecked")
-		List<ProfilDTO> profils = (List<ProfilDTO>) q.execute();
-		for(ProfilDTO p : profils){
-			out.println("profil : " + p.getUralysUID() + "<br/>");
-			for(PlayerDTO pa : p.getPlayers()){
-				out.println("player : " + pa.getPlayerUID() + " " + pa.getName()  + "<br/>");				
-			}
+		if(req.getParameter("createProfil") != null){
+			String uralysUID = Utils.generateUID();
+			ProfilDTO profil = new ProfilDTO();		
+			Key key = KeyFactory.createKey(ProfilDTO.class.getSimpleName(), uralysUID);
+			
+			profil.setKey(KeyFactory.keyToString(key));
+			profil.setUralysUID(uralysUID);
+			
+			pm.makePersistent(profil);
+			pm.close();
 		}
-		out.close();
-		
-		pm.close();
+		else if(req.getParameter("createPlayer") != null){
+			
+			String playerUID = Utils.generateUID();
+			PlayerDTO player = new PlayerDTO();		
+			Key key = KeyFactory.createKey(PlayerDTO.class.getSimpleName(), playerUID);
+			
+			player.setKey(KeyFactory.keyToString(key));
+			player.setPlayerUID(playerUID);
+			player.setName(req.getParameter("createPlayer"));
+			
+			ProfilDTO profil = pm.getObjectById(ProfilDTO.class, "128998298816601660674803");
+			profil.getPlayers().add(player);
+			
+			pm.flush();
+			pm.refreshAll();
+			
+		}
+		else if(req.getParameter("createMoves") != null){
+			
+			String moveUID = Utils.generateUID();
+			MoveDTO move = new MoveDTO();		
+			Key key = KeyFactory.createKey(MoveDTO.class.getSimpleName(), moveUID);
+			
+			//org.datanucleus.store.appengine.query.StreamingQueryResult cannot be cast to com.uralys.tribes.entities.dto.PlayerDTO
+			Query q = pm.newQuery("select from " + PlayerDTO.class.getName() + " where name == 'coco'");
+			PlayerDTO player = (PlayerDTO) q.execute();
+			
+			move.setKey(KeyFactory.keyToString(key));
+			move.setPlayer(player);
+			move.setxFrom(10);
+			move.setxTo(22);
+			move.setyFrom(10);
+			move.setyTo(12);
+			
+			
+			pm.makePersistent(move);
+			
+		}
+		else if(req.getParameter("players") != null){
+			PrintWriter out = resp.getWriter();
+			
+			Query q = pm.newQuery("select from " + ProfilDTO.class.getName());
+
+			@SuppressWarnings("unchecked")
+			List<ProfilDTO> profils = (List<ProfilDTO>) q.execute();
+			for(ProfilDTO p : profils){
+				out.println("profil : " + p.getUralysUID() + "<br/>");
+				for(PlayerDTO pa : p.getPlayers()){
+					out.println("player : " + pa.getPlayerUID() + " " + pa.getName()  + "<br/>");				
+				}
+			}
+			out.close();
+			
+			pm.close();
+		}
 	}
 }
